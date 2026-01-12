e048c599ef42b3071484c165f924c460806f8230
Zac Sweers <pandanomic@gmail.com>
2026-01-12 17:30:27 -0500

    Cap IJ counts

--- scripts/fetch-kotlin-bootstrap-versions.sh
+++ scripts/fetch-kotlin-bootstrap-versions.sh
@@ -16,7 +16,6 @@ MAVEN_BASE="${1:?Usage: $0 <maven_base_url> [count]}"
 COUNT="${2:-10}"
 
 # Get versions from maven-metadata.xml, get top N newest, then reverse for display (oldest first)
-# Note: We capture to a variable first to avoid broken pipe errors with pipefail
 ALL_VERSIONS=$(curl -sSL "$MAVEN_BASE/maven-metadata.xml" \
   | grep -oE '<version>[^<]+</version>' \
   | sed 's/<version>//; s/<\/version>//' \
